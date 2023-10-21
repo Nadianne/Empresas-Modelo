92626f253ad18f22c672ef1d897f59c4edffe9fe
Nadianne Galvão <54487179+Nadianne@users.noreply.github.com>
2023-10-21 15:43:08 -0300

    Add files via upload

--- m-compensado/Assoalhado.js
+++ m-compensado/Assoalhado.js
@@ -3,7 +3,7 @@ global.Assoalhado.addEventListener("change", function () {
     global.produtosIndicados.innerHTML = '';
     global.setSubCategoriasTitulo(["Assoalhado"])
     global.setSubcategoriaIndex(0);
-    global.setTamProdutosIndicados([4]);
+    global.setTamProdutosIndicados([10]);
     global.setImgAllPath(["../imagens-industria/Assoalhado/"]);
     global.produtosIndicados.appendChild(global.createH2(global.subCategoriasTitulo[global.subcategoriaIndex]));
 
@@ -13,7 +13,13 @@ global.Assoalhado.addEventListener("change", function () {
         'Assoalho de compensado naval 18mm - 5,50 x 2,50', 
         'Assoalho de compensado naval 18mm - 6,50 x 2,50',
         'Assoalho de compensado naval 18mm - 7,50 x 2,44',
-       
+        'Assoalho de compensado naval 18mm - 7,00 x 2,50',
+        'Assoalho de compensado naval 18mm - 7,60 x 2,44',
+        'Assoalho de compensado naval 18mm - 8,00 x 2,44',
+        'Assoalho de compensado naval 22mm - 6,20 x 2,44',
+        'Assoalho de compensado naval 22mm - 7,50 x 2,44',
+        'Assoalho de compensado naval 24mm - 6,20 x 2,44',
+        'Assoalho de compensado naval 24mm - 7,50 x 2,44',
     ],
 
     ]);
@@ -25,6 +31,13 @@ global.Assoalhado.addEventListener("change", function () {
             "Assoalho de compensado naval <span>18mm - 5,50 x 2,50 </span>",
             "Assoalho de compensado naval <span>18mm - 6,50 x 2,50 </span>",
             "Assoalho de compensado naval <span>18mm - 7,50 x 2,44 </span>",
+            "Assoalho de compensado naval <span>18mm - 7,00 x 2,50 </span>",
+            "Assoalho de compensado naval <span>18mm - 7,60 x 2,44 </span>",
+            "Assoalho de compensado naval <span>18mm - 8,00 x 2,44 </span>",
+            "Assoalho de compensado naval <span>22mm - 6,20 x 2,44 </span>",
+            "Assoalho de compensado naval <span>22mm - 7,50 x 2,44 </span>",
+            "Assoalho de compensado naval <span>24mm - 6,20 x 2,44 </span>",
+            "Assoalho de compensado naval <span>24mm - 7,50 x 2,44 </span>",
                
 
         ],
@@ -49,8 +62,36 @@ global.Assoalhado.addEventListener("change", function () {
                 "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 18mm.",
             ],
             [
-                "A Serra de fita vertical para madeira 375 Watts capacidade de 100mm - RZ-SF10/M2 da Razi realiza cortes com muita precisão e qualidade garantidas da marca, que é especializada na comercialização de equipamentos para os setores moveleiro, madeireiro e metal mecânico, se consolidando como uma das principais empresas do segmento no Brasil.",
-                "Motor 1/2CV – 220V monofásico ","Altura Máxima de Corte 100mm"," Largura máxima de corte 240mm ",
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 18mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 18mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 18mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 18mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 22mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 22mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 24mm.",
+            ],
+            [
+                "O Compensado Naval Plastificado é recomendado para o uso externo em fôrmas para concreto, com necessidade de alto índice de aproveitamentos. Pode ser usado também para tapumes, bandejas, barracos, etc.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","Comprimentos: 2500/2440 mm; <br> Larguras: 1250/1220 mm;","Espessuras: 24mm.",
             ],
           
            
--- m-compensado/Lateral.js
+++ m-compensado/Lateral.js
@@ -3,7 +3,7 @@ global.Lateral.addEventListener("change", function () {
     global.produtosIndicados.innerHTML = '';
     global.setSubCategoriasTitulo(["Lateral"])
     global.setSubcategoriaIndex(0);
-    global.setTamProdutosIndicados([4]);
+    global.setTamProdutosIndicados([17]);
     global.setImgAllPath(["../imagens-industria/Assoalhado/"]);
     global.produtosIndicados.appendChild(global.createH2(global.subCategoriasTitulo[global.subcategoriaIndex]));
 
@@ -14,6 +14,17 @@ global.Lateral.addEventListener("change", function () {
         'Assoalho de compensado lateral 14mm - 2,25 x 0,80',
         'Assoalho de compensado lateral 14mm - 2,25 x 1,00',
         'Assoalho de compensado lateral 14mm - 2,44 x 0,80',
+        'Assoalho de compensado lateral 14mm - 2,44 x 1,00',
+        'Assoalho de compensado lateral 14mm - 2,44 x 1,10',
+        'Assoalho de compensado lateral 14mm - 2,97 x 0,70',
+        'Assoalho de compensado lateral 14mm - 2,97 x 0,80',
+        'Assoalho de compensado lateral 14mm - 2,97 x 1,00',
+        'Assoalho de compensado lateral 14mm - 3,00 x 2,00',
+        'Assoalho de compensado lateral 14mm - 3,00 x 2,20',
+        'Assoalho de compensado lateral 14mm - 3,00 x 0,80 ',
+        'Assoalho de compensado lateral 14mm - 4,50 x 2,20 ',
+        'Assoalho de compensado lateral 14mm - 4,50 x 2,44 ',
+        'Assoalho de compensado lateral 14mm - 6,20 x 2,44 ',
         
     ],
    
@@ -27,7 +38,17 @@ global.Lateral.addEventListener("change", function () {
             "Assoalho de compensado lateral <span>14mm - 2,25 x 0,80 </span>",
             "Assoalho de compensado lateral <span>14mm - 2,25 x 1,00 </span>",
             "Assoalho de compensado lateral <span>14mm - 2,44 x 0,80 </span>",
-               
+            "Assoalho de compensado lateral <span>14mm - 2,44 x 1,00 </span>",
+            "Assoalho de compensado lateral <span>14mm - 2,44 x 1,10 </span>",
+            "Assoalho de compensado lateral <span>14mm - 2,97 x 0,70 </span>",  
+            "Assoalho de compensado lateral <span>14mm - 2,97 x 0,80 </span>",
+            "Assoalho de compensado lateral <span>14mm - 2,97 x 1,00 </span>",  
+            "Assoalho de compensado lateral <span>14mm - 3,00 x 2,00 </span>", 
+            "Assoalho de compensado lateral <span>14mm - 3,00 x 2,20 </span>",
+            "Assoalho de compensado lateral <span>14mm - 3,00 x 0,80 </span>",            
+            "Assoalho de compensado lateral <span>14mm - 4,50 x 2,20 </span>",
+            "Assoalho de compensado lateral <span>14mm - 4,50 x 2,44 </span>",            
+            "Assoalho de compensado lateral <span>14mm - 6,20 x 2,44 </span>",          
 
         ],
 
@@ -35,6 +56,50 @@ global.Lateral.addEventListener("change", function () {
     ]);
     global.setParagrafoPopUpAllText([
         [
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
+            [
+                "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
+                "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
+            ],
             [
                 "O compensado naval caracteriza-se por ser uma chapa de alta resistência, o que se dá por meio do entrelaçamento das lâminas em maior quantidade de camadas, podendo ser utilizado em áreas úmidas e onde se faz necessária uma grande durabilidade do material.",
                 "Desempenho: Alta resistência à abrasão superficial e grande número de reutilizações.","","",
